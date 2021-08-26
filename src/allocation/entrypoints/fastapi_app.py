97e82133ddd946ab891aed7839c7bc43e1c5c79f
Florian Kromer <florian.kromer@mailbox.org>
2021-08-26 19:19:51 +0200

    refactor to fastapi

--- src/allocation/entrypoints/fastapi_app.py
+++ src/allocation/entrypoints/fastapi_app.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from flask import Flask, jsonify, request
+from fastapi import FastAPI, Request
 
 from allocation.domain import commands
 from allocation.adapters import orm
@@ -7,12 +7,12 @@ from allocation.service_layer import messagebus, unit_of_work
 from allocation.service_layer.handlers import InvalidSku
 from allocation import views
 
-app = Flask(__name__)
+app = FastAPI()
 orm.start_mappers()
 
 
-@app.route("/add_batch", methods=["POST"])
-def add_batch():
+@app.post("/add_batch")
+def add_batch(request: Request):
     eta = request.json["eta"]
     if eta is not None:
         eta = datetime.fromisoformat(eta).date()
@@ -24,8 +24,8 @@ def add_batch():
     return "OK", 201
 
 
-@app.route("/allocate", methods=["POST"])
-def allocate_endpoint():
+@app.post("/allocate")
+def allocate_endpoint(request: Request):
     try:
         cmd = commands.Allocate(
             request.json["orderid"], request.json["sku"], request.json["qty"]
@@ -38,8 +38,8 @@ def allocate_endpoint():
     return "OK", 202
 
 
-@app.route("/allocations/<orderid>", methods=["GET"])
-def allocations_view_endpoint(orderid):
+@app.get("/itemsallocations/{orderid}")
+def allocations_view_endpoint(orderid: int):
     uow = unit_of_work.SqlAlchemyUnitOfWork()
     result = views.allocations(orderid, uow)
     if not result:
